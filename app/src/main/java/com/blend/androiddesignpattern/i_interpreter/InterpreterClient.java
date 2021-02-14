a52262df0514a470ca0f19e1457396a4e3a31bbf
BlendAndroid <1059152860@qq.com>
2021-02-14 16:06:11 +0800

    解释器模式

--- app/src/main/java/com/blend/androiddesignpattern/i_interpreter/InterpreterClient.java
+++ app/src/main/java/com/blend/androiddesignpattern/i_interpreter/InterpreterClient.java
@@ -2,6 +2,10 @@ package com.blend.androiddesignpattern.i_interpreter;
 
 import android.util.Log;
 
+/**
+ * 解释器模式：定义一种文法，并定义一个解释器，该解释器使用该表示来解释语言中的例子。
+ * Android源码中的解释器模式就是解析mxl文件，就是解释器模式。
+ */
 public class InterpreterClient {
 
     private static final String TAG = "InterpreterClient";
